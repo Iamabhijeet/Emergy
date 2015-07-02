fb6d12c4b0f95f18d84e6574baa5188649411ebd
Gabrijel Boduljak <gboduljak@outlook.com>
2015-07-03 00:18:04 +0200

    added xamarin forms app

---
+++ src/Emergy/mobile/Emergy.Mobile/Emergy.Mobile.Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Graphics.Drawables;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Emergy.Mobile.Droid
+{
+    [Activity(Label = "Emergy", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+            if ((int) Android.OS.Build.VERSION.SdkInt >= 21)
+            {
+                ActionBar.SetIcon (new ColorDrawable (Resources.GetColor (Android.Resource.Color.Transparent)));
+            }
+        }
+    }
+}
+
---
+++ src/Emergy/mobile/Emergy.Mobile/Emergy.Mobile/Pages/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emergy.Mobile.ViewModel;
+using Xamarin.Forms;
+
+namespace Emergy.Mobile.Pages
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Emergy.";
+            _mainViewModel = App.Locator.MainViewModel;
+            SetPages();
+        }
+
+
+        private void SetPages()
+        {
+          
+            this.Children.Add(_mainViewModel.SendPage);
+            this.Children.Add(_mainViewModel.ReportsPage);
+        }
+
+        private readonly MainViewModel _mainViewModel;
+
+
+
+
+    }
+}
