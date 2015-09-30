5d2e1a7b13089881f8489b814d2a0823f03b36de
Boris Borović <boris.borovic@outlook.com>
2015-09-30 10:49:40 +0200

    Fixed logout icon placement, and adjusted the item spacer

--- src/web/Emergy.Web/app/components/controllers/shell/shellController.js
+++ src/web/Emergy.Web/app/components/controllers/shell/shellController.js
@@ -8,6 +8,7 @@ app.controller(controllerId,
 function shellCtrl($scope, $rootScope, authService, authData) {
     $rootScope.background = 'background-white';
     $rootScope.authData = authData;
+
     $rootScope.logout = function() {
         authService.logout();
     };
