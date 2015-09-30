6beca3f2a2f0df544699157a17e5c482f370f38b
Gabrijel Boduljak <gboduljak@outlook.com>
2015-09-30 14:25:06 +0200

    added route restriction control

--- src/web/Emergy.Web/app/components/controllers/shell/shellController.js
+++ src/web/Emergy.Web/app/components/controllers/shell/shellController.js
@@ -8,8 +8,4 @@ app.controller(controllerId,
 function shellCtrl($scope, $rootScope, authService, authData) {
     $rootScope.background = 'background-white';
     $rootScope.authData = authData;
-
-    $rootScope.logout = function() {
-        authService.logout();
-    };
 }
