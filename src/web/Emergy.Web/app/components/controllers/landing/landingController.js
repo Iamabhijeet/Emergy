7004652c67c2b1671a21c3f228b0ec76f2672ce7
Boris Borović <boris.borovic@outlook.com>
2015-09-30 20:48:31 +0200

    Added a simple landing page

--- src/web/Emergy.Web/app/components/controllers/landing/landingController.js
+++ src/web/Emergy.Web/app/components/controllers/landing/landingController.js
@@ -3,10 +3,13 @@
 var controllerId = 'landingController';
 
 app.controller(controllerId,
-    ['$scope', '$rootScope', 'authService', 'authData', landingController]);
+    ['$scope', '$timeout', '$rootScope', '$state', 'authService', 'authData', landingController]);
 
-function landingController($scope, $rootScope, authService, authData) {
+function landingController($scope, $timeout, $rootScope, $state, authService, authData) {
     $rootScope.title = 'Landing';
+    $rootScope.background = 'background-cyan';
 
-
+    $timeout(function () {
+        $state.go('Home');
+    }, 5000);
 }
