9dfdd2aa7405b6b05e8ed3c3a011f18c82dd1975
Gabrijel Boduljak <gboduljak@outlook.com>
2015-11-06 09:57:49 +0100

    added email service

---
+++ src/Emergy.Core/Services/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.SmtpApi;
+namespace Emergy.Core.Services
+{
+    public class EmailService
+    {
+        public EmailService()
+        {
+            
+        }
+        private const string SendGridApiKey = "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";
+    }
+}
