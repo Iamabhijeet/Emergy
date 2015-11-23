e7e7f7ca08709edb1f414a8ff16bfafa0bc14cb3
Boris Borović <boris.borovic@outlook.com>
2015-11-23 23:08:09 +0100

    Added Change status menu, basic groundwork for report functionality

--- src/web/Emergy.Web/app/components/controllers/reports/reportsController.js
+++ src/web/Emergy.Web/app/components/controllers/reports/reportsController.js
@@ -3,11 +3,40 @@
 var controllerId = 'reportsController';
 
 app.controller(controllerId,
-    ['$scope', '$rootScope', 'unitsService',
+    ['$scope', '$rootScope', 'reportsService',
         'authService', 'notificationService', 'authData', reportsController]);
 
-function reportsController($scope, $rootScope, unitsService, authService, notificationService, authData) {
+function reportsController($scope, $rootScope, reportsService, authService, notificationService, authData) {
     $rootScope.title = 'Reports | Emergy';
-    $scope.isBusy = false; 
+    $scope.isBusy = false;
 
+    var loadReports = function() {
+        //Add functionality
+    }
+
+    $scope.deleteReport = function (reportId) {
+        $scope.isBusy = true;
+
+        var promise = reportsService.deleteReport(reportId);
+        promise.then(function (response) {
+            notificationService.pushSuccess("Report has been deleted!");
+                loadReports();
+            }, function (error) {
+            notificationService.pushError(error.Message);
+        })
+        .finally(function () {
+            $scope.isBusy = false;
+        });
+    }
+
+    $scope.changeStatus = function (reportId, newStatus) {
+        var promise = reportsService.changeStatus(reportId, newStatus);
+        promise.then(function (response) {
+            notificationService.pushSuccess("Status changed to " + newStatus);
+        }, function (error) {
+            notificationService.pushError(error.Message);
+        });
+    }
+
+    loadReports();
 }
---
+++ src/web/Emergy.Web/app/components/services/reports/reportsService.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+services.factory('reportsService', reportsService);
+
+reportsService.$inject = ['$http', '$q', 'serviceBase', 'authData'];
+
+function reportsService($http, $q, serviceBase, authData) {
+
+    var deleteReport = function (reportId) {
+        var deffered = $q.defer();
+        $http.delete(serviceBase + 'api/reports/delete/' + reportId)
+        .success(function (response) {
+            deffered.resolve(response);
+        })
+            .error(function (response) {
+                deffered.reject(response);
+            });
+        return deffered.promise;
+    }
+
+    var changeStatus = function (reportId, newStatus) {
+        var deffered = $q.defer();
+        $http.post(serviceBase + 'api/reports/change-status/' + reportId, newStatus)
+        .success(function (response) {
+            deffered.resolve(response);
+        })
+            .error(function (response) {
+                deffered.reject(response);
+            });
+        return deffered.promise;
+    }
+
+    var service = {
+        deleteReport: deleteReport,
+        changeStatus: changeStatus
+    };
+
+    return service;
+}
