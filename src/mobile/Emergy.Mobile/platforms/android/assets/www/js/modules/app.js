482d120c9e3a7424b9eadab514f2bf722819d9dd
Gabrijel Boduljak <gboduljak@outlook.com>
2015-11-27 01:10:52 +0100

    recreated branch mobile

---
+++ src/mobile/Emergy.Mobile/platforms/android/assets/www/js/modules/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('emergy', ['ionic', 'ng-cordova', 'emergy.controllers', 'emergy.services']);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
---
+++ src/mobile/Emergy.Mobile/www/js/modules/controllers.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="../../lib/angular/angular.js" />
+/// <reference path="../../lib/angular/angular.min.js" />
+/// <reference path="../../lib/angular/index.js" />
+
+var controllers = angular.module('emergy.controllers', ['emergy.services', 'emergy.directives']);
