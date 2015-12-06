c2c87e1b7478fbe4c99b8cc080c2fd398100886d
Boris Borović <boris.borovic@outlook.com>
2015-12-06 14:35:01 +0100

    Overhauled project structure

---
+++ src/mobile/Emergy.Mobile/www/app/app.router.js
@@ -0,0 +1,61 @@
+﻿app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('tab', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: function () {
+              if (ionic.Platform.isAndroid()) {
+                  return "app/views/tabsAndroid/tabsAndroid.html";
+              }
+              return "app/views/tabsIos/tabsIos.html";
+          }
+      })
+        .state('tab.home', {
+            url: '/home',
+            views: {
+                'tab-home': {
+                    templateUrl: 'app/views/home/home.html',
+                    controller: 'homeController'
+                }
+            }
+        })
+        .state('tab.reports', {
+            url: '/reports',
+            views: {
+                'tab-reports': {
+                    templateUrl: 'app/views/reports/reports.html',
+                    controller: 'reportsController'
+                }
+            }
+        })
+        .state('tab.report-details', {
+            url: '/report/:reportId',
+            views: {
+                'tab-report-details': {
+                    templateUrl: 'app/views/reportDetails/reportDetails.html',
+                    controller: 'reportController'
+                }
+            }
+        })
+
+    .state('profile', {
+        url: '/profile/:userId',
+        views: {
+            'tab-profile': {
+                templateUrl: 'app/views/profile/profile.html',
+                controller: 'profileController'
+            }
+        }
+    })
+    .state('tab.login', {
+        url: '/login',
+        views: {
+            'tab-login': {
+                templateUrl: 'app/views/login/login.html',
+                controller: 'loginController'
+            }
+        }
+    });
+
+    $urlRouterProvider.otherwise('/tab/home');
+});
--- src/mobile/Emergy.Mobile/www/js/modules/controllers.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="../../lib/angular/angular.js" />
-/// <reference path="../../lib/angular/angular.min.js" />
-/// <reference path="../../lib/angular/index.js" />
-
-var controllers = angular.module('emergy.controllers', ['emergy.services', 'emergy.directives']);
