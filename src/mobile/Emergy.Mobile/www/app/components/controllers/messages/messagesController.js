70ba8b5d111adbfd7a5559b256eb8d0885f12890
Boris Borović <boris.borovic@outlook.com>
2016-01-09 15:22:31 +0100

    Implemented messages, can't test yet, but should work

--- src/mobile/Emergy.Mobile/www/app/components/controllers/messages/messagesController.js
+++ src/mobile/Emergy.Mobile/www/app/components/controllers/messages/messagesController.js
@@ -3,7 +3,54 @@
 var controllerId = 'messagesController';
 
 app.controller(controllerId,
-    ['$scope', '$stateParams', '$rootScope', 'authService', 'notificationService', 'messagesService', messagesController]);
+    ['$scope', '$stateParams', '$timeout', '$ionicScrollDelegate', '$rootScope', 'authService', 'notificationService', 'messagesService', messagesController]);
 
-function messagesController($scope, $stateParams, $rootScope, authService, notificationService, messagesService) {
+function messagesController($scope, $stateParams, $timeout, $ionicScrollDelegate, $rootScope, authService, notificationService, messagesService) {
+    var isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+    $scope.message = "";
+    $scope.messages = [];
+
+    $scope.senderId = $stateParams.senderId;
+
+    var loadMessages = function () {
+        var promise = messagesService.getMessages(String($scope.senderId));
+        promise.then(function (messages) {
+            $scope.messages = messages;
+        }, function (error) {
+            notificationService.pushError("Error has happened while loading messages.");
+        }).finally(function () {
+            $ionicScrollDelegate.scrollBottom(true);
+        });
+    };
+
+    $scope.sendMessage = function(message) {
+        var promise = messagesService.createMessage(message, $scope.senderId);
+        promise.then(function(messageId) {
+            loadMessages();
+        }, function(error) {
+            notificationService.displayErrorPopup("There has been an error sending a message.", "Ok");
+        }).finally(function() {
+            delete $scope.message;
+        });
+    };
+
+    $scope.inputUp = function () {
+        if (isIOS) $scope.data.keyboardHeight = 216;
+        $timeout(function () {
+            $ionicScrollDelegate.scrollBottom(true);
+        }, 200);
+
+    };
+
+    $scope.inputDown = function () {
+        if (isIOS) $scope.data.keyboardHeight = 0;
+        $ionicScrollDelegate.resize();
+        $ionicScrollDelegate.scrollBottom(true);
+    };
+
+    $scope.closeKeyboard = function () {
+        cordova.plugins.Keyboard.close();
+    };
+
+    loadMessages();
 }
--- src/mobile/Emergy.Mobile/www/app/components/services/messages/messagesService.js
+++ src/mobile/Emergy.Mobile/www/app/components/services/messages/messagesService.js
@@ -16,8 +16,40 @@ function messagesService($http, $q, serviceBase, authData) {
         return deffered.promise;
     };
 
+    var getMessages = function (userId) {
+        var deffered = $q.defer();
+        $http.get(serviceBase + 'api/messages/get-chats/messages/' + userId)
+        .success(function (response) {
+            deffered.resolve(response);
+        })
+            .error(function (response) {
+                deffered.reject(response);
+            });
+        return deffered.promise;
+    };
+
+    var createMessage = function (message, userId) {
+        var deffered = $q.defer();
+
+        var messageModel = {
+            Content: message,
+            TargetId: userId
+        }
+
+        $http.post(serviceBase + 'api/messages/create', messageModel)
+        .success(function (response) {
+            deffered.resolve(response);
+        })
+            .error(function (response) {
+                deffered.reject(response);
+            });
+        return deffered.promise;
+    };
+
     var service = {
-        getMessagedUsers: getMessagedUsers
+        getMessagedUsers: getMessagedUsers, 
+        getMessages: getMessages, 
+        createMessage: createMessage
     };
 
     return service;
