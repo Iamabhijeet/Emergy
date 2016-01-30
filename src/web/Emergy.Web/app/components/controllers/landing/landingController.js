cd10fb9be0d2da7cb811d2544869090b43ef566f
Boris Borović <boris.borovic@outlook.com>
2016-01-30 14:33:08 +0100

    Added landing page with basic elements WIP, some parts not responsive yet

--- src/web/Emergy.Web/app/components/controllers/landing/landingController.js
+++ src/web/Emergy.Web/app/components/controllers/landing/landingController.js
@@ -3,11 +3,16 @@
 var controllerId = 'landingController';
 
 app.controller(controllerId,
-    ['vm', '$timeout', '$rootScope', '$state', 'authService', 'authData', landingController]);
+    ['$rootScope', '$state', 'authData', landingController]);
 
-function landingController($scope, $timeout, $rootScope, $state, authService, authData) {
+function landingController($rootScope, $state, authData) {
     $rootScope.title = 'Landing';
     $rootScope.background = 'background-white';
     
+    if (authData.loggedIn && authData.isAdmin()) {
+        $state.go('Units');
+    } else if (authData.loggedIn && authData.isClient()) {
+        $state.go('ClientDashboard', authData.userId);
+    }
 
 }
